2fdfd9efd130c48b1ca1bace00d35855888ca19f
Stuart Traynor <sttrayno@cisco.com>
2020-10-31 15:44:05 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,12 +2,12 @@
 apt-get update -y && apt-get upgrade -y
 apt-get install python3 -y
 apt-get install python3-pip -y && apt-get install python3-venv -y
+mv ./telegraf/* /etc/telegraf/telegraf.d
+mkdir /etc/telegraf/scripts
+mv ./scripts/* /etc/telegraf/scripts
 python3 -m venv env3
 mkdir /opt/telegraf
 cd /opt/telegraf
 python3 -m venv env3
 source env3/bin/activate
 pip3 install requests
-mv ./telegraf/* /etc/telegraf/telegraf.d
-mkdir /etc/telegraf/scripts
-mv ./scripts/* /etc/telegraf/scripts
