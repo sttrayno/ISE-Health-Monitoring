63d5bcc786548d1409ecfb8818dba51e839b04ec
Stuart Traynor <sttrayno@cisco.com>
2020-10-31 12:41:56 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 !#/bin/bash
-apt-get update && apt-get upgrade -y
+apt-get update -y && apt-get upgrade -y
 apt-get install python3 -y
 apt-get install python3-pip && apt-get install python3-venv -y
 pip3 install requests
