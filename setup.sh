48f93b31ba7bea0fe5731771cbe7432cb278106c
Stuart Traynor <sttrayno@cisco.com>
2020-11-01 12:44:25 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -12,3 +12,7 @@ python3 -m venv env3
 source env3/bin/activate
 pip3 install requests
 cd /etc/telegraf/telegraf.d
+grep -rl "{{ISENODE}}" . | xargs sed -i 's/{{ISENODE}}/new_string/g'
+grep -rl "{{PORT}}" . | xargs sed -i 's/{{PORT}}/9060/g'
+grep -rl "{{USER}}" . | xargs sed -i 's/{{USER}}/new_string/g'
+grep -rl "{{PASSWORD}}" . | xargs sed -i 's/{{PASSWORD}}/new_string/g'
